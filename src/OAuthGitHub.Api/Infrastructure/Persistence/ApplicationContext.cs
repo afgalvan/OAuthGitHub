52551159ddde107f711b8b828e1091eab1f83f52
Andres Galvan <afgalvan@unicesar.edu.co>
2021-07-30 19:21:17 -0500

    infra: move application context to infrastructure

--- src/OAuthGitHub.Api/Infrastructure/Persistence/ApplicationContext.cs
+++ src/OAuthGitHub.Api/Infrastructure/Persistence/ApplicationContext.cs
@@ -1,7 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using OAuthGitHub.Api.Domain;
 
-namespace OAuthGitHub.Api.Data
+namespace OAuthGitHub.Api.Infrastructure.Persistence
 {
     public class ApplicationContext : DbContext
     {
@@ -9,6 +9,16 @@ namespace OAuthGitHub.Api.Data
 
         public ApplicationContext(DbContextOptions options) : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .HasIndex(user => user.Name)
+                .IsUnique();
+            modelBuilder.Entity<User>()
+                .HasIndex(user => user.Email)
+                .IsUnique();
         }
     }
 }
